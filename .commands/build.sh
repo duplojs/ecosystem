61bcd9999052e8d0c86fd7dc89ddad7f498379c8
mathcovax <campani.mathieu@gmail.com>
2026-09-12 13:17:40 +0200

    fix(hf): tsconfig

--- .commands/build.sh
+++ .commands/build.sh
@@ -2,9 +2,20 @@
 
 set -euo pipefail
 
+printf "\n${GREEN}@duplojs/code-config${RESET}\n"
 pnpm --filter @duplojs/code-config build
+
+printf "\n${GREEN}@duplojs/lang${RESET}\n"
 pnpm --filter @duplojs/lang build
+
+printf "\n${GREEN}@duplojs/server${RESET}\n"
 pnpm --filter @duplojs/server build
+
+printf "\n${GREEN}@duplojs/tools${RESET}\n"
 pnpm --filter @duplojs/tools build
+
+printf "\n${GREEN}@duplojs/json-web-token${RESET}\n"
 pnpm --filter @duplojs/json-web-token build
+
+printf "\n${GREEN}@duplojs/playwright${RESET}\n"
 pnpm --filter @duplojs/playwright build
